9eeca9a3fbf03521fe991c4f1c45d27a23ededfd
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-06-17 18:20:20 -0300

    Projeto criado, com comunicação com MySQL

---
+++ src/main/java/br/com/lmf/BancoLM/BancoLmApplication.java
@@ -0,0 +1,13 @@
+package br.com.lmf.BancoLM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoLmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoLmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lmf/BancoLM/BancoLmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lmf.BancoLM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoLmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
