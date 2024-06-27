006c7021f0c363e0175682b29dabbb5941639f78
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-06-26 22:28:01 -0300

    Criado relacionamento com o banco de dados

--- src/main/java/br/com/lmf/BancoLM/entities/Cliente.java
+++ src/main/java/br/com/lmf/BancoLM/entities/Cliente.java
@@ -1,14 +1,39 @@
 package br.com.lmf.BancoLM.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cliente")
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String nome;
+	
+	@Column(nullable = false, unique = true)
 	private Integer cpfCnpj;
+	
+	@Column(nullable = false)
 	private Integer telefone;
+	
+	@Column(nullable = false, unique = true)
 	private String email;
+	
+	@Column(nullable = false, unique = true)
 	private String senha;
 	
+	@ManyToOne
+	@JoinColumn(name = "endereco_id", referencedColumnName = "id")
 	private Endereco endereco;
 	
 	public Cliente() {
--- src/main/java/br/com/lmf/BancoLM/entities/Endereco.java
+++ src/main/java/br/com/lmf/BancoLM/entities/Endereco.java
@@ -1,32 +1,62 @@
 package br.com.lmf.BancoLM.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_endereco")
 public class Endereco {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
 	private String cep;
+	
+	@Column(nullable = false, unique = true)
 	private String logradouro;
-	private String complemento;
-	private String unidade;
-	private String uf;
-	private String ibge;
-	private String gia;
-	private String ddd;
-	private String siafi;
+	
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 	
 	public Endereco() {
 		
 	}
 
-	public Endereco(String cep, String logradouro, String complemento, String unidade, String uf, String ibge,
-			String gia, String ddd, String siafi) {
+	public Endereco(Long id, String cep, String logradouro, String complemento, String unidade, String bairro,
+			String localidade, String uf, String ibge, String gia, String ddd, String siafi) {
+		this.id = id;
 		this.cep = cep;
 		this.logradouro = logradouro;
 		this.complemento = complemento;
 		this.unidade = unidade;
+		this.bairro = bairro;
+		this.localidade = localidade;
 		this.uf = uf;
 		this.ibge = ibge;
 		this.gia = gia;
 		this.ddd = ddd;
 		this.siafi = siafi;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getCep() {
@@ -59,6 +89,22 @@ public class Endereco {
 
 	public void setUnidade(String unidade) {
 		this.unidade = unidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
 	}
 
 	public String getUf() {
@@ -100,6 +146,9 @@ public class Endereco {
 	public void setSiafi(String siafi) {
 		this.siafi = siafi;
 	}
+
+
+	
 	
 	
 
